20477f63142629a1179f4d852ba4247dca484374
Hörnkvist <robin.hornkvist@sogeti.com>
2025-02-03 14:22:07 +0100

    added dockerfile

--- Program.cs
+++ Program.cs
@@ -4,9 +4,8 @@ var app = builder.Build();
 var users = new List<User> { new(Guid.NewGuid(), "Robin"), new (Guid.NewGuid(), "Stefan") };
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/users", () => users);
+app.MapGet("/users", (string? query) => query is not null ? users.Where(x => x.Name.Contains(query, StringComparison.InvariantCultureIgnoreCase)) : users);
 
 app.Run();
-
 
 record User(Guid Id, string Name);
