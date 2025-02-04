32bd425b61315541d87abd8e7904d5c2bd015ac2
Hörnkvist <robin.hornkvist@sogeti.com>
2025-02-04 11:32:06 +0100

    testing slots

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var app = builder.Build();
 
 var users = new List<User> { new(Guid.NewGuid(), "Robin"), new (Guid.NewGuid(), "Stefan") };
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello updated world!");
 app.MapGet("/users", (string? query) => query is not null ? users.Where(x => x.Name.Contains(query, StringComparison.InvariantCultureIgnoreCase)) : users);
 
 app.Run();
