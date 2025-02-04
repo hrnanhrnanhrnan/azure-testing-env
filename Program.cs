848069b175aa0326ee747f232d2ed05a30fc8f5d
Hörnkvist <robin.hornkvist@sogeti.com>
2025-02-04 11:55:44 +0100

    changed env

--- Program.cs
+++ Program.cs
@@ -3,7 +3,38 @@ var app = builder.Build();
 
 var users = new List<User> { new(Guid.NewGuid(), "Robin"), new (Guid.NewGuid(), "Stefan") };
 
-app.MapGet("/", () => "Hello updated world!");
+app.MapGet("/", () => """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+        .container {
+            background-color: black;
+            color: white;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HELLO</title>
+</head>
+<body>
+    <div class="container">
+        <h1>
+            Hello world
+        </h1>
+    </div>
+</body>
+</html>
+""");
 app.MapGet("/users", (string? query) => query is not null ? users.Where(x => x.Name.Contains(query, StringComparison.InvariantCultureIgnoreCase)) : users);
 
 app.Run();
