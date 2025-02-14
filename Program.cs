b657361acc87890a2ce3d2b0d5ada6efc215bd8e
Hörnkvist <robin.hornkvist@sogeti.com>
2025-02-14 10:56:35 +0100

    added logo

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 var keyVaultUri = new Uri(builder.Configuration["AzureKeyVault:Uri"]!);
+var credential = new DefaultAzureCredential();
 
-builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential());
+builder.Configuration.AddAzureKeyVault(keyVaultUri, credential);
 builder.Services.AddLogging();
 
 var applicationInsightsConnectionString = builder.Configuration["ApplicationInsights"];
@@ -30,6 +31,7 @@ builder.Services.AddOpenTelemetry()
     );
 
 var app = builder.Build();
+app.UseStaticFiles();
 
 var users = new List<User> { 
     new(Guid.NewGuid(), "Robin"), 
@@ -48,6 +50,7 @@ var users = new List<User> {
 app.MapGet("/", () => Results.Content("""
 <!DOCTYPE html>
 <html lang="en">
+<link rel="icon" href="images/hamburger.svg">
 <head>
     <script src="https://unpkg.com/htmx.org@2.0.4" integrity="sha384-HGfztofotfshcF7+8n44JQL2oJmowVChPTg48S+jvZoztPfvwD79OC/LTtG6dMp+" crossorigin="anonymous"></script>
     <style>
@@ -72,13 +75,20 @@ app.MapGet("/", () => Results.Content("""
             flex-direction: column;
             gap: 10px;
         }
+        #logo {
+            height: 40px;
+        }
+        .highlighter {
+            color: #ffbf5e;
+        }
     </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HELLO</title>
+    <title>UserSearcher</title>
 </head>
 <body>
     <div class="container">
+        <img id="logo" src="images/hamburger.svg" alt="">
         <h1>
             User searcher
         </h1>
@@ -92,6 +102,11 @@ app.MapGet("/", () => Results.Content("""
 </body>
 </html>
 """, "text/html"));
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Request: {context.Request.Path}");
+    await next();
+});
 
 app.MapGet("/users", (string? query, [FromServices]ILogger<Program> logger, HttpContext context) => 
 {
@@ -106,11 +121,11 @@ app.MapGet("/users", (string? query, [FromServices]ILogger<Program> logger, Http
 
     if (results.Count == 0)
     {
-        logger.LogInformation("Query {Query} resulted in 0 users", query);
+        logger.LogInformation("Query '{Query}' resulted in 0 users", query);
         return "";
     }
 
-    logger.LogInformation("Query {Query} resulted in {Count} users", query, results.Count);
+    logger.LogInformation("Query '{Query}' resulted in {Count} users", query, results.Count);
 
     return $$"""
         {{string.Join("\n", results.Select(TurnUserIntoHtml))}}
@@ -119,7 +134,7 @@ app.MapGet("/users", (string? query, [FromServices]ILogger<Program> logger, Http
 
 static string TurnUserIntoHtml(User user)
     => $$"""
-    <div>- id: {{user.Id}}, name: {{user.Name}}</div>
+    <div><span class="highlighter">-</span> id: {{user.Id}}, name: <span class="highlighter">{{user.Name}}</span></div>
     """;
 
 app.Run();
